ede93ca878842e7e4fabad21d1c15e2d33de0d01
MJ Habib <mj.habib4@gmail.com>
2024-06-02 22:29:41 +0330

    convert the app to a statefulWidget so we can add some functionality to it later on

--- 01_first_flutter_app/lib/src/app.dart
+++ 01_first_flutter_app/lib/src/app.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // all the names before ':' which start with lower case letters are 'named parameters' passed to their parent classes, and after ':' we have names/classes which begin with an upper-case letter.
       home: Scaffold(
+        body: Text('$counter'),
         appBar: AppBar(
             title: const Text(
           'Lets\'s see some images',
         )),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
         ),
       ),
     );
