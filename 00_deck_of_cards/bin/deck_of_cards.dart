a46e2a3bdabb5dd536a6d636c5189089d20bd318
MJ Habib <mj.habib4@gmail.com>
2024-06-02 13:57:57 +0330

    deal a new hand

--- 00_deck_of_cards/bin/deck_of_cards.dart
+++ 00_deck_of_cards/bin/deck_of_cards.dart
@@ -1,8 +1,16 @@
 void main() {
   var deck = Deck();
   deck.shuffle();
+
+  print('All the Spades: ');
   print(deck.cardWithSuit('Spades'));
   // anytime we want to print out an instance of a class, if we call the toString function, we can print out any custom message we want
+
+  print('\n Deal hand: ');
+  print(deck.deal(5));
+
+  print('\n Remaining cards: ');
+  print(deck.cards);
 }
 
 class Deck {
@@ -33,6 +41,14 @@ class Deck {
     //   return card.suit == suit;
     // });
     return cards.where((card) => card.suit == suit);
+  }
+
+  deal(int handSize) {
+    var hand = cards.sublist(0, handSize);
+    // when we 'deal' a new 'hand of cards', the sublist is not gonna remove the 'hand' from our original list
+    cards = cards.sublist(handSize);
+    // that's why we need to call the sublist again, from handSize to 'end', so we can remove our 'hand' from the original list
+    return hand;
   }
 }
 
