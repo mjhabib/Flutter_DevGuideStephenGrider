142edee2a9423ac80deb226d4d071067f8cc967a
MJ Habib <mj.habib4@gmail.com>
2024-06-02 21:48:36 +0330

    add a FloatingActionButton widget

--- 01_first_flutter_app/lib/main.dart
+++ 01_first_flutter_app/lib/main.dart
@@ -10,11 +10,16 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // all the names before ':' which start with lower case letters are 'named parameters' passed to their parent classes, and after ':' we have names/classes which begin with an upper-case letter.
       home: Scaffold(
         appBar: AppBar(
             title: const Text(
           'Lets\'s see some images',
         )),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
       ),
     );
   }
