de8f04f46574703cb14fba1ea1e681bc2232b28a
MJ Habib <mj.habib4@gmail.com>
2024-06-04 21:56:10 +0330

    add form validation functionality

--- 02_login_stateful/lib/screens/login_screen.dart
+++ 02_login_stateful/lib/screens/login_screen.dart
@@ -8,21 +8,25 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  // this GlobalKey allow us to reference any methods/properties associated with it, like form validation
+
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: const EdgeInsets.all(20),
         child: Form(
+            key: formKey,
             child: Column(
-          children: [
-            emailField(),
-            passField(),
-            Container(
-              margin: const EdgeInsets.only(bottom: 25),
-            ),
-            submitButton()
-          ],
-        )));
+              children: [
+                emailField(),
+                passField(),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                ),
+                submitButton()
+              ],
+            )));
   }
 
   Widget emailField() {
@@ -30,6 +34,14 @@ class _LoginScreenState extends State<LoginScreen> {
       keyboardType: TextInputType.emailAddress,
       decoration: const InputDecoration(
           labelText: 'Email: ', hintText: 'mail@test.com'),
+      validator: (value) {
+        if (value != null) {
+          if (!value.contains('@')) {
+            return 'Please enter a valid email';
+          }
+        }
+        return null;
+      },
     );
   }
 
@@ -38,10 +50,22 @@ class _LoginScreenState extends State<LoginScreen> {
       obscureText: true,
       decoration:
           const InputDecoration(labelText: 'Password: ', hintText: '******'),
+      validator: (value) {
+        if (value != null) {
+          if (value.length < 6) {
+            return 'Password must be at least 6 characters';
+          }
+        }
+        return null;
+      },
     );
   }
 
   Widget submitButton() {
-    return ElevatedButton(onPressed: () {}, child: const Text('Submit'));
+    return ElevatedButton(
+        onPressed: () {
+          formKey.currentState?.validate();
+        },
+        child: const Text('Submit'));
   }
 }
