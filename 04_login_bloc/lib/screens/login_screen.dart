1d51ee50e9d168a425026e65cbb5fd35a61b5af2
MJ Habib <mj.habib4@gmail.com>
2024-06-13 18:18:11 +0330

    design login_screen page

--- 04_login_bloc/lib/screens/login_screen.dart
+++ 04_login_bloc/lib/screens/login_screen.dart
@@ -5,6 +5,35 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          emailField(),
+          passField(),
+          Container(margin: const EdgeInsets.only(bottom: 20)),
+          submitButton()
+        ],
+      ),
+    );
   }
+}
+
+Widget emailField() {
+  return const TextField(
+    keyboardType: TextInputType.emailAddress,
+    decoration:
+        InputDecoration(hintText: 'mail@you.com', labelText: 'Email Address:'),
+  );
+}
+
+Widget passField() {
+  return const TextField(
+    obscureText: true,
+    decoration: InputDecoration(hintText: '******', labelText: 'Password:'),
+  );
+}
+
+Widget submitButton() {
+  return ElevatedButton(onPressed: () {}, child: const Text('Submit'));
 }
