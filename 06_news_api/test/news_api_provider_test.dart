e1c3a922cca6450b5f7591260153307bc572c859
MJ Habib <mj.habib4@gmail.com>
2024-06-16 20:46:54 +0330

    test the app

--- 06_news_api/test/news_api_provider_test.dart
+++ 06_news_api/test/news_api_provider_test.dart
@@ -5,11 +5,29 @@ import 'package:http/testing.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids', () {
+  test('FetchTopIds returns a list of ids', () async {
     // Setup the test cast
-    const sum = 1 + 3;
-
+    // const sum = 1 + 3;
     // Expectation
-    expect(sum, 4);
+    // expect(sum, 4);
+
+    final newsApi = NewsApiProvider();
+    // Instead of sending a real request to the server, we send a mock request to see how our app behaves. In that case we get an instance response because we don't have to wait for an outside server, and also we have the ability to control the data.
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem returns an item', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode({'id': 123}), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+    expect(item.id, 123);
   });
 }
