8a665fab1b96e6a774da8c8d9a44bbae12d629ef
MJ Habib <mj.habib4@gmail.com>
2024-06-16 22:31:09 +0330

    create a new sqlite DB

---
+++ 06_news_api/lib/resources/news_db_provider.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import '/models/item_model.dart';
+
+class NewsDbProvider {
+  late Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    // a reference to the directory on our device
+    final path = join(documentsDirectory.path, 'items.db');
+    // a reference to the db itself
+    db = await openDatabase(
+      // either create a newDb or open one that already exists
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        // """""" = multiline string
+        newDb.execute("""
+          CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            text TEXT,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+      """);
+      },
+    );
+  }
+}
