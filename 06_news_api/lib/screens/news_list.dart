52b938b4104e3a004d5787476e605afaf1ffa71f
MJ Habib <mj.habib4@gmail.com>
2024-06-20 20:02:58 +0330

    add news_list screen

---
+++ 06_news_api/lib/screens/news_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  const NewsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Top News'),
+      ),
+      body: const Text('Show some news here!'),
+    );
+  }
+}
