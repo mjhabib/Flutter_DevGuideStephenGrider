112691e13ea0b294ed58e8e8fa5065f4faede760
MJ Habib <mj.habib4@gmail.com>
2024-06-21 10:20:30 +0330

    initial the stories_bloc implementation (get/fetch ids) to test the app

--- 06_news_api/lib/blocs/stories_bloc.dart
+++ 06_news_api/lib/blocs/stories_bloc.dart
@@ -1,1 +1,19 @@
-class StoriesBloc {}
+import 'package:news_api/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class StoriesBloc {
+  final _repository = Repository();
+  final _topIds =
+      PublishSubject<List<int>>(); // == like StreamController in BLoC
+
+  Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topIds.close();
+  }
+}
