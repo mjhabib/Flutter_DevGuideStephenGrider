6f31bd9bac3395ba689c1cd4d3d13b7adadc21af
MJ Habib <mj.habib4@gmail.com>
2024-06-23 00:24:32 +0330

    add printOptions functionality to the terminal

--- 07_dart_prompter/bin/main.dart
+++ 07_dart_prompter/bin/main.dart
@@ -1,9 +1,17 @@
+import 'package:dart_prompter_mj/src/option.dart';
 import 'package:dart_prompter_mj/src/terminal.dart';
 
 void main() {
   var terminal = Terminal();
+
   terminal.clearScreen();
-  terminal.printPrompt('print this on console');
-  var input = terminal.collectInput();
-  terminal.printPrompt('You typed: $input');
+
+  var optionsList = [
+    Option('I want red', '#f00'),
+    Option('I want blue', '#00f')
+  ];
+
+  terminal.printOptions(optionsList);
+  var response = terminal.collectInput();
+  terminal.printPrompt('You choose: $response');
 }
--- 07_dart_prompter/lib/src/option.dart
+++ 07_dart_prompter/lib/src/option.dart
@@ -1,1 +1,6 @@
-class Option {}
+class Option {
+  late String label;
+  late dynamic value;
+
+  Option(this.label, this.value);
+}
