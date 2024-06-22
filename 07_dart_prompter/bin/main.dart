65eb68cd26bc3dfffd6ee5629edbea0c70964c6b
MJ Habib <mj.habib4@gmail.com>
2024-06-22 19:07:24 +0330

    collect user's input using stdin.readLineSync

--- 07_dart_prompter/bin/main.dart
+++ 07_dart_prompter/bin/main.dart
@@ -4,4 +4,6 @@ void main() {
   var terminal = Terminal();
   terminal.clearScreen();
   terminal.printPrompt('print this on console');
+  var input = terminal.collectInput();
+  terminal.printPrompt('You typed: $input');
 }
