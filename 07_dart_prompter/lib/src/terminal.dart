03dfc603ea450e862b9ecb1f3e70fb892426ae47
MJ Habib <mj.habib4@gmail.com>
2024-06-25 10:51:02 +0330

    refactor terminal code from a for loop to a forEach for simplicity

--- 07_dart_prompter/lib/src/terminal.dart
+++ 07_dart_prompter/lib/src/terminal.dart
@@ -7,12 +7,19 @@ class Terminal {
   }
 
   void printOptions(List<Option> options) {
-    for (var i = 0; i < options.length; i++) {
-      stdout.writeln('[$i] - ${options[i].label}');
-    }
+    // for (var i = 0; i < options.length; i++) {
+    //   stdout.writeln('[$i] - ${options[i].label}');
+    // }
+
+    // Alternative way to deal with our list of options
+    // we turn/convert our list to a map then iterate through it
+    // the sole reason we used asMap is because we care about the index of that option, otherwise we could just iterate through our list without any indexes
+    options.asMap().forEach((index, value) {
+      stdout.writeln('[$index] - ${value.label}');
+    });
 
     stdout.writeln('Enter a choice: \n');
-    stdout.write('< ');
+    stdout.write('> ');
   }
 
   String? collectInput() {
