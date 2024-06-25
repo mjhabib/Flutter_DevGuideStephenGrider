d13bb7891017d941da9b62567c166b0d0bba16df
MJ Habib <mj.habib4@gmail.com>
2024-06-25 14:08:16 +0330

    error handling using try-catch block

--- 07_dart_prompter/lib/src/prompter.dart
+++ 07_dart_prompter/lib/src/prompter.dart
@@ -12,11 +12,19 @@ class Prompter {
 
     // if I define my Terminal here, every time I call the ask method it's gonna create an instance of that and later on throw it away, which means the waste of resources, that's why we define the Terminal instance outside of our class or method. If we define it inside our class, by calling the class each time, we're gonna create multiple instance of that Terminal which we don't need.
 
-    _terminal.clearScreen();
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
 
     final input = _terminal.collectInput();
-    return options[int.parse(input!)].value;
+
+    try {
+      _terminal.clearScreen();
+      _terminal.printPrompt('You choose: ');
+      return options[int.parse(input!)].value;
+    } catch (e) {
+      _terminal.clearScreen();
+      _terminal.printPrompt('You entered an invalid input. Try again! \n');
+      return ask(prompt, options);
+    }
   }
 }
