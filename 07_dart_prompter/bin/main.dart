3919f7c7b23bd291a7352258660368da7c559f54
MJ Habib <mj.habib4@gmail.com>
2024-06-25 11:56:41 +0330

    add functionality to the prompter class instead of doing all the work inside the main class

--- 07_dart_prompter/bin/main.dart
+++ 07_dart_prompter/bin/main.dart
@@ -1,17 +1,18 @@
 import 'package:dart_prompter_mj/src/option.dart';
+import 'package:dart_prompter_mj/src/prompter.dart';
 import 'package:dart_prompter_mj/src/terminal.dart';
 
 void main() {
   var terminal = Terminal();
+  var prompter = Prompter();
 
   terminal.clearScreen();
 
-  var optionsList = [
-    Option('I want red', '#f00'),
-    Option('I want blue', '#00f')
-  ];
+  var options = [Option('I want red', '#f00'), Option('I want blue', '#00f')];
+  String prompt = 'This is my question?';
 
-  terminal.printOptions(optionsList);
+  prompter.ask(prompt, options);
+
   var response = terminal.collectInput();
   terminal.printPrompt('You choose: $response');
 }
--- 07_dart_prompter/lib/src/prompter.dart
+++ 07_dart_prompter/lib/src/prompter.dart
@@ -1,1 +1,16 @@
-class Prompter {}
+import 'terminal.dart';
+import 'option.dart';
+
+Terminal terminal = Terminal();
+
+class Prompter {
+  // Terminal terminal = Terminal();
+  ask(String prompt, List<Option> options) {
+    // Terminal terminal = Terminal();
+
+    // if I define my Terminal here, every time I call the ask method it's gonna create an instance of that and later on throw it away, which means the waste of resources, that's why we define the Terminal instance outside of our class or method. If we define it inside our class, by calling the class each time, we're gonna create multiple instance of that Terminal which we don't need.
+
+    terminal.printPrompt(prompt);
+    terminal.printOptions(options);
+  }
+}
