eefd3a8340f6594861076bd852e7c29342c8c162
MJ Habib <mj.habib4@gmail.com>
2024-06-25 13:58:09 +0330

    refactor the code to use final/const/private instead of var

--- 07_dart_prompter/bin/main.dart
+++ 07_dart_prompter/bin/main.dart
@@ -1,18 +1,20 @@
 import 'package:dart_prompter_mj/src/option.dart';
 import 'package:dart_prompter_mj/src/prompter.dart';
-import 'package:dart_prompter_mj/src/terminal.dart';
 
 void main() {
-  var terminal = Terminal();
-  var prompter = Prompter();
+  final prompter = Prompter();
 
-  terminal.clearScreen();
+  // terminal.clearScreen();
+  // even that we did not define a new Terminal instance in this file, we still have access to it because by importing the Prompter here, we have access to everything in that file, including the Terminal instance. To prevent this, we did mark that Terminal as private by adding '_' before that variable name.
 
-  var options = [Option('I want red', '#f00'), Option('I want blue', '#00f')];
-  String prompt = 'This is my question?';
+  final options = [Option('I want red', '#f00'), Option('I want blue', '#00f')];
+  final prompt = 'This is my question?';
 
-  prompter.ask(prompt, options);
-
-  var response = terminal.collectInput();
-  terminal.printPrompt('You choose: $response');
+  final usersPick = prompter.ask(prompt, options);
+  print(usersPick);
 }
+
+
+// var: we can reassign it in the future
+// final: the variable can't be re-assigned BUT we can still modify its values
+// const: a compile time constant which can't be re-assigned, or changing values in the future. 
--- 07_dart_prompter/lib/src/prompter.dart
+++ 07_dart_prompter/lib/src/prompter.dart
@@ -1,7 +1,9 @@
 import 'terminal.dart';
 import 'option.dart';
 
-Terminal terminal = Terminal();
+final Terminal _terminal = const Terminal();
+// the final keyword won't allow anyone to misuse this instance by re-assigning a new value to it.
+// const won't allow to create more than one instance of Terminal
 
 class Prompter {
   // Terminal terminal = Terminal();
@@ -10,7 +12,11 @@ class Prompter {
 
     // if I define my Terminal here, every time I call the ask method it's gonna create an instance of that and later on throw it away, which means the waste of resources, that's why we define the Terminal instance outside of our class or method. If we define it inside our class, by calling the class each time, we're gonna create multiple instance of that Terminal which we don't need.
 
-    terminal.printPrompt(prompt);
-    terminal.printOptions(options);
+    _terminal.clearScreen();
+    _terminal.printPrompt(prompt);
+    _terminal.printOptions(options);
+
+    final input = _terminal.collectInput();
+    return options[int.parse(input!)].value;
   }
 }
